b18d3f6637a7d29b5da0e1fcf6f91267b95443c0
everbrez <everbrez@gmail.com>
2019-03-27 00:16:03 +0800

    [style] update sass

--- others/test.js
+++ others/test.js
@@ -1,34 +1,10 @@
-function quick3Way(arr = [], left = 0, right = arr.length) {
-  if(left >= right) return
+const fs = require('fs');
+const { Writable, Readable } = require('stream');
+const inStream = new Readable();
 
-  let i = left + 1
-  let lf = left
-  let gt = right
-  const v = arr[left]
-  while(i < gt) {
-    if(arr[i] < v) exchange(arr, i++, lf++)
-    else if(arr[i] > v) exchange(arr, i, gt--)
-    else i++
-  }
+inStream.push('ABCDEFGHIJKLM');
+inStream.push('NOPQRSTUVWXYZ');
 
-  quick3Way(arr, left, lf - 1)
-  quick3Way(arr, gt + 1, right)
-  return arr
-}
+inStream.push(null); // 没有更多数据了
 
-function exchange(arr, i, j) {
-  [arr[i], arr[j]] = [arr[j], arr[i]]
-}
-
-
-function random() {
-  return Math.random().toString().slice(2)
-}
-
-function test(sort) {
-  const data = random().split('').map(num => +num + +random().substr(0, 2))
-  const res = sort(data)
-  console.log(res === data.sort((a, b) => a - b))
-}
-
-for (let i = 0; i < 50; i++) test(quick3Way)
+inStream.pipe(process.stdout);
